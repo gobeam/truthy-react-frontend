5a6dcfd58ed4184acc51aad642bfc0151f44b349
Roshan Ranabhat <roshanranabhat11@gmail.com>
2022-01-17 00:19:30 +0545

    fix: Reset password token issue resolved

--- app/containers/ResetPassword/constants.js
+++ app/containers/ResetPassword/constants.js
@@ -10,4 +10,4 @@ export const ADD_VALIDATION_ERROR =
 export const ASYNC_START = 'containers/ResetPassword/ASYNC_START';
 export const ASYNC_END = 'containers/ResetPassword/ASYNC_END';
 export const SET_FORM_VALUES = 'containers/ResetPassword/SET_FORM_VALUES';
-export const SET_RESET_CODE = 'containers/ResetPassword/SET_FORM_VALUES';
+export const SET_RESET_CODE = 'containers/ResetPassword/SET_RESET_CODE';
