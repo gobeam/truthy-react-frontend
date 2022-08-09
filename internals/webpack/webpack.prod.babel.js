60a28b7725a835818c7538ba7f656a59ca0afb43
Roshan Ranabhat <roshanranabhat11@gmail.com>
2022-08-09 14:26:38 +0545

    fix: update icon webpack configuration for production env

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -27,7 +27,6 @@ module.exports = require('./webpack.base.babel')({
     minimizer: [
       new TerserPlugin({
         terserOptions: {
-          warnings: false,
           compress: {
             comparisons: false,
           },
@@ -91,20 +90,20 @@ module.exports = require('./webpack.base.babel')({
     }),
 
     new WebpackPwaManifest({
-      name: 'RSP CMS',
-      short_name: 'RSP',
-      description: 'RSP Frontend application!',
+      name: 'Truthy CMS',
+      short_name: 'Truthy',
+      description: 'Truthy Frontend application!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
       inject: true,
       ios: true,
       icons: [
         {
-          src: path.resolve('app/assets/images/icons/icon.png'),
+          src: path.resolve('app/assets/images/icons/icon-512x512.png'),
           sizes: [72, 96, 128, 144, 192, 384, 512],
         },
         {
-          src: path.resolve('app/assets/images/icons/icon.png'),
+          src: path.resolve('app/assets/images/icons/icon-512x512.png'),
           sizes: [120, 152, 167, 180],
           ios: true,
         },
